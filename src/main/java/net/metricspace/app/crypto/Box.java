d0bc28df6bfcba93f32c662d0dc6295f6149ce2e
Eric McCorkle <emc2@metricspace.net>
2016-11-24 08:29:03 -0500

    Basic JAXB integration

---
+++ src/main/java/net/metricspace/app/crypto/Box.java
@@ -0,0 +1,12 @@
+package net.metricspace.app.crypto;
+
+import net.metricspace.app.crypto.jaxb.AbstractBox;
+
+/**
+ * Authenticated symmetric encryption primitive.  Boxes are a wrapper
+ * for symmetric encryption with authentication.
+ */
+public class Box<T> extends AbstractBox {
+
+    //public T unlock() {}
+}
